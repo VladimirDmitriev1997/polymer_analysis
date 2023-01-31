7910e2b5a1dc58128e7d64265c290f711d0af5dd
VladimirDmitriev1997 <62100997+VladimirDmitriev1997@users.noreply.github.com>
2023-01-31 15:48:23 +0100

    Update tesnor_analysis.py

--- tesnor_analysis.py
+++ tesnor_analysis.py
@@ -3,17 +3,19 @@ import numpy as np
 def custom_diag(X, trans=False):
     """
     Average and variance over the diagonals
-    
+    X (timesteps, dim1, dim2)
     if trans = True - compute lower and upper diagonals
     else: assume that lower triangle is zero
     """
-    assert X.shape[0] != X.shape[1]
+    assert X.shape[1] == X.shape[2]
     avg_distance = []
     std_distance = []
     for i in range(X.shape[0]):
         if trans:
-            diag = np.diagonal(X, i).reshape(-1)
-        avg_distance.append(diag.mean())
-        std_distance.append(diag.std())
+            diag = np.vstack((np.diagonal(X, i, axis1=1, axis2=2),np.diagonal(X, -i, axis1=1, axis2=2))).reshape(X.shape[0],-1)
+        else:
+            diag = np.diagonal(X, i, axis1=1, axis2=2).reshape(X.shape[0],-1)
+        avg_distance.append(diag.mean(axis=1))
+        std_distance.append(diag.std(axis=1))
     
     return np.array(avg_distance), np.array(std_distance)
